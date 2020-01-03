47ef6cc26d5ded13a3c929eda0e4a139eafbf835
Denny Kluge <denny.kluge@gmail.com>
2020-01-03 22:41:51 +0100

    part 5 of tutorial finished

--- stock-ui/src/main/java/de/diskostu/reactivespringboot/stockui/ChartController.java
+++ stock-ui/src/main/java/de/diskostu/reactivespringboot/stockui/ChartController.java
@@ -1,11 +1,18 @@
 package de.diskostu.reactivespringboot.stockui;
 
+import de.diskostu.reactivespringboot.stockclient.WebClientStockClient;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ChartController {
+
     @FXML
     public LineChart<String, Double> chart;
+    private WebClientStockClient webClientStockClient;
+
+    public ChartController(WebClientStockClient webClientStockClient) {
+        this.webClientStockClient = webClientStockClient;
+    }
 }
