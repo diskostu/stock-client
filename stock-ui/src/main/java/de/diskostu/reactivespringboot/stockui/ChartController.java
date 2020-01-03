a29d678a60a8f8f0245140b7746332bf6ec7566b
Denny Kluge <denny.kluge@gmail.com>
2020-01-03 23:06:51 +0100

    part 6 of tutorial finished

--- stock-ui/src/main/java/de/diskostu/reactivespringboot/stockui/ChartController.java
+++ stock-ui/src/main/java/de/diskostu/reactivespringboot/stockui/ChartController.java
@@ -1,18 +1,44 @@
 package de.diskostu.reactivespringboot.stockui;
 
+import de.diskostu.reactivespringboot.stockclient.StockPrice;
 import de.diskostu.reactivespringboot.stockclient.WebClientStockClient;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
 import org.springframework.stereotype.Component;
 
+import java.util.function.Consumer;
+
 @Component
-public class ChartController {
+public class ChartController implements Consumer<StockPrice> {
 
     @FXML
     public LineChart<String, Double> chart;
     private WebClientStockClient webClientStockClient;
+    private ObservableList<XYChart.Data<String, Double>> seriesData = FXCollections.observableArrayList();
 
     public ChartController(WebClientStockClient webClientStockClient) {
         this.webClientStockClient = webClientStockClient;
+    }
+
+    @FXML
+    public void initialize() {
+        final String symbol = "SYMBOL";
+
+        final ObservableList<XYChart.Series<String, Double>> data = FXCollections.observableArrayList();
+        data.add(new XYChart.Series<>(symbol, seriesData));
+        chart.setData(data);
+
+        webClientStockClient.pricesFor(symbol).subscribe(this);
+    }
+
+    @Override
+    public void accept(StockPrice stockPrice) {
+        Platform.runLater(() ->
+                seriesData.add(new XYChart.Data<String, Double>(String.valueOf(stockPrice.getTime().getSecond()),
+                        stockPrice.getPrice())));
     }
 }
